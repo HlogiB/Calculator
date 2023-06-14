9a5a44eba81b201c21e50206908695307a808940
Lehlogonolo Bale <Bale.forthwith@gmail.com>
2023-06-14 14:30:40 +0200

    Update CALCULATOR.py

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,10 +1,10 @@
 from tkinter import *
-<<<<<<< HEAD
+
 
 #Hello thereew we are the master 
 #Aowa hlogi rocks bathong
 #So Master is not apperaring this side
->>>>>>> hlogi
+
 root=Tk()
 
 e=Entry(root,width=50,borderwidth=5)
